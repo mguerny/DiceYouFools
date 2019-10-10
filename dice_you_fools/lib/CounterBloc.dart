01cfc46fbb9d9e88edd668a5d5b43bdb7698af54
Marin <marin.guerny@laposte.net>
2019-10-10 16:46:12 +0200

    Bloc archi + Home page background

---
+++ dice_you_fools/lib/CounterBloc.dart
@@ -0,0 +1,19 @@
+import 'dart:async';
+import 'BlocProvider.dart';
+
+class CounterBloc extends BlocBase {
+  StreamController streamController = StreamController<int>.broadcast();
+
+  Sink get counterSink => streamController.sink;
+  Stream<int> get counterStream => streamController.stream;
+
+  int counter = 0;
+  incrementCounter() {
+    counterSink.add(++counter);
+  }
+
+  @override
+  void dispose() {
+    streamController.close();
+  }
+}
