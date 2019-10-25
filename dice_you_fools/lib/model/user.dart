c0b46a4e79cebee4290db4eb2b834b1e9b8df64b
romain petit <contact.romain.petit@gmail.com>
2019-10-25 14:54:16 +0200

    add cognito auth, apple sign in, rename app

--- dice_you_fools/lib/model/user.dart
+++ dice_you_fools/lib/model/user.dart
@@ -1,9 +1,29 @@
-class User {
+import 'package:amazon_cognito_identity_dart/cognito.dart';
 
-   final String id;
-   String name;
-   String email;
-   
-  User({this.id, this.name, this.email});
+class User {
+  String email;
+  String name;
+  String password;
+  bool confirmed = false;
+  bool hasAccess = false;
+
+  User({String email, String name, String password}){
+    this.email = email;
+    this.name = name;
+    this.password = password;
+  }
+
+  /// Decode user from Cognito User Attributes
+  factory User.fromUserAttributes(List<CognitoUserAttribute> attributes) {
+    final user = User();
+    attributes.forEach((attribute) {
+      if (attribute.getName() == 'email') {
+        user.email = attribute.getValue();
+      } else if (attribute.getName() == 'name') {
+        user.name = attribute.getValue();
+      }
+    });
+    return user;
+  }
 
 }
--- dice_you_fools/lib/user_repository.dart
+++ dice_you_fools/lib/user_repository.dart
@@ -1,9 +1,23 @@
 import 'package:meta/meta.dart';
+import 'package:dice_you_fools/Model/User.dart';
+import 'package:amazon_cognito_identity_dart/cognito.dart';
+import 'package:apple_sign_in/apple_sign_in.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class UserRepository {
 
 
   ConnectionType _connectionType = ConnectionType.nothing;
+  //CognitoUserPool _userPool;
+  CognitoUser _cognitoUser;
+  CognitoUserSession _session;
+  CognitoCredentials credentials;
+  CognitoUser _currentUser;
+
+  String get userName {
+    return _currentUser?.getUsername();
+  }
 
 
 
@@ -11,25 +25,133 @@ class UserRepository {
     return "_currentUser?.uid";
   }
 
-
-
   _reset(){
-    //_currentUser = null;
+    _currentUser = null;
     _connectionType = ConnectionType.nothing;
   }
 
-  Future<String> authenticate({@required String username, @required String password,}) async {
+  Future<String> signInWithApple() async{
+    print("SignInWithApple");
+    final AuthorizationResult result = await AppleSignIn.performRequests([
+      AppleIdRequest(requestedScopes: [Scope.email, Scope.fullName])
+    ]);
 
+    switch (result.status) {
+      case AuthorizationStatus.authorized:
+
+      // Store user ID
+        await FlutterSecureStorage()
+            .write(key: "userId", value: result.credential.user);
+        return "token";
+        break;
+
+      case AuthorizationStatus.error:
+        print("Sign in failed: ${result.error.localizedDescription}");
+        break;
+
+      case AuthorizationStatus.cancelled:
+        print('User cancelled');
+        break;
+    }
+    return null;
+  }
+
+  Future<String> signUp({@required String email, @required String password,}) async {
+    final userPool = new CognitoUserPool(
+        'us-east-1_vlikgVjot', '6tthsuuoeuvt9h35cvv49enng2');
+    final userAttributes = [
+      new AttributeArg(name: 'name', value: "Romain"),
+      new AttributeArg(name: 'phone_number', value: "+33601734665"),
+    ];
+    print("SignUp USER REPO");
+    var data;
+    try {
+      data = await userPool.signUp(email, password,userAttributes: userAttributes);
+      print("DATA ======> " + data);
+    } on CognitoUserConfirmationNecessaryException catch (e) {
+      print("confirmation necessaire !!!!!");
+    }
+    catch (e) {
+      print("ERROR : ");
+      print(e);
+      return "error";
+    }
+    return 'token';
+  }
+
+  Future<String> smsValidation({@required String email, @required String code}) async {
+    final userPool = new CognitoUserPool(
+        'us-east-1_vlikgVjot', '6tthsuuoeuvt9h35cvv49enng2');
+
+    final cognitoUser = new CognitoUser(
+        email, userPool, storage: userPool.storage);
+
+    bool registrationConfirmed = false;
+    try {
+      registrationConfirmed = await cognitoUser.confirmRegistration(code);
+    } catch (e) {
+      print(e);
+    }
+    print(registrationConfirmed);
+  }
+
+
+  Future<String> authenticate({@required String email, @required String password,}) async {
+    final userPool = new CognitoUserPool(
+        'us-east-1_vlikgVjot', '6tthsuuoeuvt9h35cvv49enng2');
+    final cognitoUser = new CognitoUser(
+        email, userPool);
+    final authDetails = new AuthenticationDetails(
+        username: email, password: password);
+    try {
+      this._session = await cognitoUser.authenticateUser(authDetails);
+
+    } on CognitoUserNewPasswordRequiredException catch (e) {
+      print("-------------------------------------------------");
+      print("CognitoUserNewPasswordRequiredException"+ e.toString());
+      bool passwordChanged = false;
+      try {
+        passwordChanged = await cognitoUser.changePassword(
+            password, 'Rwjafc6884!');
+      } catch (e) {
+        print(e);
+      }
+      print(passwordChanged);
+      print("-------------------------------------------------");
+      // handle New Password challenge
+    } on CognitoUserMfaRequiredException catch (e) {
+      print("CognitoUserMfaRequiredException"+ e.toString());
+      // handle SMS_MFA challenge
+    } on CognitoUserSelectMfaTypeException catch (e) {
+      print("CognitoUserSelectMfaTypeException"+ e.toString());
+      // handle SELECT_MFA_TYPE challenge
+    } on CognitoUserMfaSetupException catch (e) {
+      print("CognitoUserMfaSetupException"+ e.toString());
+      // handle MFA_SETUP challenge
+    } on CognitoUserTotpRequiredException catch (e) {
+      print("CognitoUserTotpRequiredException"+ e.toString());
+      // handle SOFTWARE_TOKEN_MFA challenge
+    } on CognitoUserCustomChallengeException catch (e) {
+      print("CognitoUserCustomChallengeException"+ e.toString());
+      // handle CUSTOM_CHALLENGE challenge
+    } on CognitoUserConfirmationNecessaryException catch (e) {
+      print("CognitoUserConfirmationNecessaryException"+ e.toString());
+      // handle User Confirmation Necessary
+    } catch (e) {
+      print("Auth error"+ e.toString());
+    }
+    //print(session.getAccessToken().getJwtToken());
+    print("SESSION : " + this._session.getAccessToken().getJwtToken());
     return 'token';
   }
 
 
 
-  /*_succeedConnection(FirebaseUser user, ConnectionType connectionType){
+  _succeedConnection(CognitoUser user, ConnectionType connectionType){
     _connectionType = connectionType;
     _currentUser = user;
-    handleFirstConnection(user.uid);
-  }*/
+    handleFirstConnection(user.getUsername());
+  }
 
   Future handleFirstConnection(String uid) async {
     /*CollectionReference users = Firestore.instance.collection("user");
@@ -97,5 +219,5 @@ class UserRepository {
 }
 
 enum ConnectionType {
-  google, model, nothing
+  model, nothing
 }
