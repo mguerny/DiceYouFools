52a02118be872339689214f5724a2de7f1946213
romain petit <contact.romain.petit@gmail.com>
2019-10-31 10:11:56 +0100

    update podfile, spalsh screen updated

--- dice_you_fools/lib/splash/splash_page.dart
+++ dice_you_fools/lib/splash/splash_page.dart
@@ -1,38 +1,53 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class SplashPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color.fromRGBO(25, 255, 217, 1),
-      body: Center (
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Image.asset(
-              'assets/images/clock.png',
-              width: 320,
-              height: 320,
-              /*color: Colors.red,
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: ExactAssetImage('assets/images/gandalf.jpg'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: BackdropFilter(
+          filter: ImageFilter.blur(sigmaX: 3.0, sigmaY: 3.0),
+          child: Container(
+            decoration: BoxDecoration(color: Colors.white.withOpacity(0.0)),
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Image.asset(
+                    'assets/images/dice.png',
+                    width: 120,
+                    height: 120,
+                    /*color: Colors.red,
               colorBlendMode: BlendMode.multiply,*/
-              fit: BoxFit.cover,
-            ),
-            RichText(
-              text: TextSpan(
-                children: <TextSpan>[
-                  TextSpan(text: "My"),
-                  TextSpan(text: "Emoji", style: TextStyle(fontWeight: FontWeight.w300),),
+                    fit: BoxFit.cover,
+                  ),
+                  RichText(
+                    text: TextSpan(
+                      children: <TextSpan>[
+                        TextSpan(text: "Dice"),
+                        TextSpan(text: "YouFools !", style: TextStyle(fontWeight: FontWeight.w300),),
+                      ],
+                      style: TextStyle(
+                        fontFamily: "Quicksand",
+                        fontWeight: FontWeight.w500,
+                        fontSize: 62.0,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
                 ],
-                style: TextStyle(
-                  fontFamily: "Quicksand",
-                  fontWeight: FontWeight.w500,
-                  fontSize: 62.0,
-                  color: Colors.white,
-                ),
               ),
             ),
-          ],
+          ),
         ),
       ),
     );
